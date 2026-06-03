b2d037af07bee6c2e939107d0d914f8efde0239e
tanvibhardwaj123 <tanvibhardwaj2006@gmail.com>
2026-06-03 22:39:39 +0530

    Create simple_interest.py

---
+++ labwork/lab_operation_3rd june/simple_interest.py
@@ -0,0 +1,13 @@
+[15:03, 03/06/2026] Tanvi....😘: #Wap to calculate simple interest and validate the data if necessary
+principle = float(input("enter the principle amount"))
+Time = float(input("enter the time "))
+Rate = float(input("enter the Rate "))
+if(principle<0):
+    exit("principle not negative")
+if(Time<0):
+    exit("Time cannot be Negative")
+if(Rate<0):
+    exit("Rate cannot be Negative")
+Simple_Interest = (principle*Time*Rate)/100 
+print("Simple Interest is",Simple_Interest)
+
