208e0e9500e87f28c5b0f509813d2b27ffda37f6
tanvibhardwaj123 <tanvibhardwaj2006@gmail.com>
2026-06-04 22:43:33 +0530

    Refactor battery charging level display program
    Refactor battery charging program to improve readability and structure. Added comments for clarity.

--- classwork/iteration_4th June/battery_charging.py
+++ classwork/iteration_4th June/battery_charging.py
@@ -1,11 +1,24 @@
-#program for display battery charging level
-charging_level = 20
-electricity_status = True
-while(charging_level <= 100 ):
-    if(charging_level):
-        print("Battery charging level:", charging_level, "%")   
-        charging_level += 10
+#Program to display battery charging level
+#Battery starts charging from 20%
+#Increase charging level by 10% until it reaches 100%
+#Display "Full Charge" when charging is complete
+
+charging_level=20
+electricity_status=True
+
+#---------------------------------------------------
+#charging battery
+
+while(charging_level<=100):
+
+    if(electricity_status):
+        print("Battery Charging Level =",charging_level,"%")
+        charging_level+=10
+
     else:
         break
-    #----------------------------------------------------
-print("full charge")
+
+#---------------------------------------------------
+#displaying charging status
+
+print("Full Charge")
